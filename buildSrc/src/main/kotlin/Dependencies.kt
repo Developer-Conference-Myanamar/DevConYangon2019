a48e94d86a9cc5b5f0e266408de39287faeb4fa3
vincent-paing <uriaungkyawpaing@gmail.com>
2019-12-21 13:55:41 +0630

    config: Up verison to 1.1.1

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -8,7 +8,7 @@ object BuildConfig {
 
   private const val versionMajor = 1
   private const val versionMinor = 1
-  private const val versionPatch = 0
+  private const val versionPatch = 1
   private const val versionBuild = 0
 
   const val versionName =
